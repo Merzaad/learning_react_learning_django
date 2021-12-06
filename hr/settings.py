4d51e13c421e09b218b7550b83baf972a7c14e09
Merzaad <88736874+Merzaad@users.noreply.github.com>
2021-12-06 13:36:51 +0330

    media

--- hr/settings.py
+++ hr/settings.py
@@ -109,6 +109,10 @@ USE_TZ = True
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_URL = '/static/'
 
+MEDIA_URL= '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
