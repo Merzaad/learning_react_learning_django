44b34ca66e93804f2ac54cfe9e4e44e7b5454812
Merzaad <Merzaad@gmail.com>
2022-01-01 18:28:12 +0330

    post loop

--- react/news/src/App.js
+++ react/news/src/App.js
@@ -2,18 +2,42 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 function App() {
+    var post_box = [
+        {
+            text: "For less than $300 you're getting one of our favorite gaming monitors of 2021, and a mighty impressive specsheet to boot.",
+            title: "Dell's mighty 32-inch 165Hz gaming monitor is cheaper today than on Black Friday",
+            update: "January 19, 2022",
+            tag: "news"
+        },
+        {
+            text: "Kill bizarre demons and use their blood as paint in mad roguelike Brutal Orchestra",
+            title: "Kill bizarre demons and use their blood as paint in mad roguelike Brutal Orchestra",
+            update: "January 13, 2022",
+            tag: "ad"
+        },
+        {
+            text: "Here's a game about selling maple syrup to soup up your trashy car for street racing",
+            title: "Deliver pizza to live the Quebecois dream in Mon Bazou.",
+            update: "January 1, 2022",
+            tag: "news"
+        },
+    ]
     function post() {
-        const text = "For less than $300 you're getting one of our favorite gaming monitors of 2021, and a mighty impressive specsheet to boot.";
-        const title = "Dell's mighty 32-inch 165Hz gaming monitor is cheaper today than on Black Friday";
-        const update = "December 1, 2022"
-
+        let a = [];
+        for (let i of post_box) {
+            a.push(
+                <div class="p-2 mt-3" >
+                    <p class="h2 pb-2"><a href="#" class="lnk">{i.title}</a></p>
+                    <p class="text-secondary">Updated {i.update}<span class="badge bg-secondary mx-2"><a class="lnk" href="#">{i.tag}</a></span></p>
+                    <p>{i.text}</p>
+                </div >
+            )
+        }
         return (
-            <div class="p-2 mt-3">
-                <p class="h2 pb-2"><a href="#" class="lnk">{title}</a></p>
-                <p class="text-secondary">Updated {update}<span class="badge bg-secondary mx-2"><a class="lnk" href="#">News</a></span></p>
-                <p>{text}</p>
+            <div>
+                {a}
             </div>
-        )
+            )
     }
     ReactDOM.render(post(), document.getElementById('post'));
 };
