44110cd5656359b00cbf7085003d5cc4cb81051e
Merzaad <Merzaad@gmail.com>
2022-01-13 15:08:22 +0330

    test

--- react/news/src/apps/test.js
+++ react/news/src/apps/test.js
@@ -1,13 +1,36 @@
 function TestBtn() {
     function padd() {
-        let a = (a, b) => {
-            return a + b
+        function co (name, lastname) {
+            this.name = name;
+            this.lastname = lastname;
+            this.fullname = function () {
+                return this.name + this.lastname
+            };
         }
+        const x = () => {
+            let a=new co("m", "h");
+            return a.fullname()
+        }
+
+        function sum(...args) {
+            let a = 'HEllo'
+            for (let i of args) {
+                if (i.includes(a.toLowerCase())) {
+                    alert('yess')
+                }
+                else {
+                    setTimeout(function () { alert("no") }, 1000);
+                };
+            }
+            
+        };
+        sum("i said hello","i said no")
+    
         document.getElementById('right').classList.add('p-5')
-        //console.log(typeof a)
+         
     }
     return (
-        <button id='btn' className="btn bg-dark text-white" onClick={padd}>test</button>
+        <button id='btn' className="btn bg-dark text-white" onClick={padd}>console</button>
     )
 };
 
