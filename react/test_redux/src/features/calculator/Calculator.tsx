075723353b1ba188c2a1a4db8d20150dd25cdbbf
Mehrzad Hosseini <Merzaad@gmail.com>
2022-02-04 05:20:07 +0330

    Delete react/test_redux directory

--- react/test_redux/src/features/calculator/Calculator.tsx
+++
@@ -1,6 +0,0 @@
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import {selectValue} from './calculatorSlice'
-export const Calculator = () => {
-    const test = useAppSelector(selectValue)
-    return (<div>{test}</div>)
-}
